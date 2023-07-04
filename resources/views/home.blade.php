8d34de37b80d8dd0015d3ddcd55b5e45c27851da
Arya Veda Setyanindito <vedakhansa@gmail.com>
2023-07-04 13:14:01 +0700

    style content in home page home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,27 +14,22 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ asset('image/memberi.jpg') }}" height="400"
-                        style="object-fit: cover;
-"
-                        class="d-block rounded-4 w-100 m-auto" alt="...">
+                    <img src="{{ asset('image/memberi.jpg') }}" height="400" style="object-fit: cover;"
+                        class="d-block rounded-2 w-100 m-auto" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <p>Luangkan Sedikit, Ubah Banyak.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="{{ asset('image/larm-rmah-AEaTUnvneik-unsplash.jpg') }}" height="400"
-                        style="object-fit: cover;
-"
-                        class="d-block rounded-4 w-100 m-auto" alt="...">
+                        style="object-fit: cover;" class="d-block rounded-2 w-100 m-auto" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <p>Membuat Dunia Lebih Baik, Langkah Sedekah.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="{{ asset('image/christian-dubovan-Y_x747Yshlw-unsplash.jpg') }}" height="400"
-                        style="object-fit: cover;"
-                        class="d-block rounded-4 w-100 m-auto" alt="...">
+                        style="object-fit: cover;" class="d-block rounded-2 w-100 m-auto" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <p>Di Setiap Sedekah, Ada Harapan yang Berkembang.</p>
                     </div>
@@ -42,7 +37,7 @@
             </div>
 
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev" style="fill: white; transform: scale(2);">
+                data-bs-slide="prev" style="fill: white;">
                 <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512">
                     <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                     <path
@@ -51,13 +46,55 @@
             </button>
 
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next" style="fill: white; transform: scale(2);">
+                data-bs-slide="next" style="fill: white;">
                 <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512">
                     <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                     <path
                         d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z" />
                 </svg>
             </button>
+        </div>
+
+        {{-- Home Content --}}
+        <div class="row" style="margin-top: 80px">
+            <div class="col">
+                <img src="{{ asset('image/christian-dubovan-Y_x747Yshlw-unsplash.jpg') }}" height="270"
+                    style="object-fit: cover;" class="d-block rounded-2 w-100 m-auto" alt="...">
+            </div>
+
+            <div class="col" >
+                <h1>Manfaat Bersedekah</h1>
+                <p class="mt-3 lh-lg">Sedekah adalah salah satu amalan yang dianjurkan dalam agama dan memiliki manfaat yang besar bagi diri kita
+                    dan orang lain. Melalui sedekah, kita dapat membantu mereka yang membutuhkan, serta memberikan dampak positif
+                    bagi masyarakat.</p>
+                <a href="/donation" class="btn btn-primary mt-3">Donasi Sekarang</a>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 80px">
+            <div class="col">
+                <h1>Memberi Harapan</h1>
+                <p class="mt-3 lh-lg">Bersedekah memberikan harapan kepada mereka yang sedang menghadapi kesulitan. Saat kita memberikan bantuan, kita tidak hanya memberikan bantuan materiil, tetapi juga memberikan dukungan emosional dan spiritual</p>
+            </div>
+
+            <div class="col">
+                <img src="{{ asset('image/larm-rmah-AEaTUnvneik-unsplash.jpg') }}" height="270"
+                    style="object-fit: cover;" class="d-block rounded-2 w-100 m-auto" alt="...">
+            </div>
 
         </div>
-    @endsection
+
+        <div class="row" style="margin-top: 80px">
+            <div class="col">
+                <img src="{{ asset('image/memberi.jpg') }}" height="270"
+                    style="object-fit: cover;" class="d-block rounded-2 w-100 m-auto" alt="...">
+            </div>
+
+            <div class="col">
+                <h1>Membangun Rasa Syukur</h1>
+                <p class="mt-3 lh-lg">Melalui bersedekah, kita dapat mengembangkan rasa syukur yang lebih dalam terhadap apa yang kita miliki. Saat kita melihat kehidupan orang lain yang kurang beruntung, kita menjadi lebih menghargai berkat dan rezeki yang telah kita terima</p>
+            </div>
+        </div>
+
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,25 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Donasiku</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body style="font-family: 'Poppins'; background-color: #F0F2F5">
 
     <!-- Navbar -->
-    <nav class="navbar fixed-top navbar-expand-lg bg-white navbar-light py-3">
+    <nav class="navbar fixed-top navbar-expand-lg bg-white navbar-light py-3 shadow-sm">
         <div class="container">
             <a class="navbar-brand d-flex" href="/">
                 <img src="{{ asset('image/logo.png') }}" alt="" width="30" height="30">
                 <span style="font-size: 23.5px; margin-left: -4px; font-weight: 600;">onasiKu</span>
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse container" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('home') }}">Home</a>
@@ -50,8 +52,12 @@
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
